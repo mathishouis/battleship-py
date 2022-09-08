ce577b8f585b1a8ac630d484ad6d921e342b13c0
Mathis HOUIS <mathishouis@gmail.com>
2022-09-08 18:37:02 +0200

    Grid building

--- grid.py
+++ grid.py
@@ -2,15 +2,17 @@ from tile import Tile
 
 class Grid:
     def __init__(self, x, y):
+        self.tiles = []
         self.x = x
         self.y = y
         self.__draw__()
         
     def __draw__(self):
-        self.tiles = []
         for x in range(self.x):
-            if len(self.tiles) < x: tiles[x] = []
+            if len(self.tiles) - 1 < x: self.tiles.append([])
             for y in range(self.y):
-                if len(self.tiles[x]) < y: tiles[x][y] = []
-                tiles[x][y] = "cc"
+                if len(self.tiles[x]) - 1 < y: self.tiles[x].append([])
+                self.tiles[x][y] = Tile(x, y)
+
+        print(self.tiles)
                 
